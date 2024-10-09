585fb193cec03aeac3243d71c67e45be72643732
Christopher Robin Chase <christopherchase011@gmail.com>
2024-10-09 17:32:23 +0800

    updated github and personal website link

--- components/Sections/FooterSection.vue
+++ components/Sections/FooterSection.vue
@@ -6,11 +6,11 @@
       All rights reserved &copy; {{ new Date().getFullYear() }} - WeatherApp
     </p>
     <p class="tw-flex tw-items-center tw-justify-center tw-gap-2">
-      <span class="tw-text-blue-500">Christopher Robin Chase</span>
+      <nuxt-link to="https://www.polywork.com/chrischase011" target="_blank"><span class="tw-text-blue-500">Christopher Robin Chase</span></nuxt-link>
       <nuxt-link
         to="https://github.com/chrischase011"
         target="_blank"
-        class="tw-text-black tw-flex tw-items-center"
+        class="tw-text-gray-500 tw-flex tw-items-center"
         ><v-icon icon="mdi-github"
       /></nuxt-link>
     </p>
